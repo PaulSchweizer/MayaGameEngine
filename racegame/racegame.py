7d05731428721809f9407d51c801b5728cc2b657
Paul Schweizer <paulschweizer@gmx.net>
2016-05-05 19:12:35 -0700

    Update racegame.py

--- racegame/racegame.py
+++ racegame/racegame.py
@@ -1,4 +1,6 @@
 import os
+import time
+from PySide import QtGui
 
 import pymel.core as pm
 from maya import mel
@@ -24,12 +26,67 @@ class RaceGameWidget(base_class, form_class):
         super(RaceGameWidget, self).__init__()
         self.setupUi(self)
 
-        # 1. Load a list of race courses from a file location
+        # Load all courses
+        self._load_courses()
 
         # 2. Load a list of Vehicles from file location
+        self._load_vehicles()
 
         # 5. Show the high score
     # end def
+
+    def _load_courses(self):
+        """@todo documentation for _load_courses."""
+        base = os.path.join(os.path.dirname(__file__), 'resource', 'courses')
+        for course in os.listdir(base):
+            if course.endswith('.jpg'):
+                image = os.path.join(base, course)
+                item = QtGui.QListWidgetItem()
+                icon = QtGui.QIcon(QtGui.QPixmap(image))
+                item.setIcon(icon)
+                item.setText(course[:-4])
+                self.course_lst.addItem(item)
+            # end if
+        # end for
+    # end def _load_courses
+
+    def _load_vehicles(self):
+        """@todo documentation for _load_vehicles."""
+        base = os.path.join(os.path.dirname(__file__), 'resource', 'vehicles')
+        for course in os.listdir(base):
+            if course.endswith('.jpg'):
+                image = os.path.join(base, course)
+                item = QtGui.QListWidgetItem()
+                icon = QtGui.QIcon(QtGui.QPixmap(image))
+                item.setIcon(icon)
+                item.setText(course[:-4])
+                self.vehicle_lst_a.addItem(item)
+
+                image = os.path.join(base, course)
+                item = QtGui.QListWidgetItem()
+                icon = QtGui.QIcon(QtGui.QPixmap(image))
+                item.setIcon(icon)
+                item.setText(course[:-4])
+                self.vehicle_lst_b.addItem(item)
+            # end if
+        # end for
+    # end def _load_vehicles
+
+    def selected_course(self):
+        """@todo documentation for selected_course."""
+        file_name = '%s.ma' % self.course_lst.selectedItems()[0].text()
+        return os.path.join(os.path.dirname(__file__), 'resource', 'courses', file_name)
+    # end def selected_course
+
+    def selected_vehicles(self):
+        """@todo documentation for selected_vehicles."""
+        file_name_a = '%s.ma' % self.vehicle_lst_a.selectedItems()[0].text()
+        file_name_b = '%s.ma' % self.vehicle_lst_a.selectedItems()[0].text()
+        return [os.path.join(os.path.dirname(__file__), 'resource', 'vehicles', file_name_a),
+                os.path.join(os.path.dirname(__file__), 'resource', 'vehicles', file_name_b)]
+    # end def selected_vehicles
+
+
 # end class RaceGameWidget
 
 
@@ -41,30 +98,62 @@ class RaceGameUI(gameengine.GameEngineUI):
     """
 
     def __init__(self):
-        """Initialize the RaceGameUI."""
+        """Initialize the RaceGameUI"""
         super(RaceGameUI, self).__init__(RaceGameWidget())
-
-        # 8. Display the elapsed Time
-        # -----> Create a Counter game_object
-        # 9. Enable/Disable Controls in GameEngine
-
     # end def __init__
 
     def on_start(self):
         """@todo documentation for on_start."""
-        # Create the GameManager
-        GameManager(pm.createNode('transform', n='GameManager'))
 
-        # Create the Vehicles
-        vehicle.Vehicle('C_main_CTL',
-                        'C_steeringAxis_CTL',
-                        'C_steeringWheel_CTL',
-                        )
+        # Create the GameManager
+        game_manager = GameManager(pm.createNode('transform', n='GameManager'))
+
+        course = self.inner_widget.selected_course()
+        vehicles = self.inner_widget.selected_vehicles()
+        pm.newFile(f=True)
+        pm.importFile(course)
+
+        # Create Colliders
+        gameobject.CurveCollider('L_edge_CRV')
+        gameobject.CurveCollider('R_edge_CRV')
+
+        for v, namespace in zip(vehicles, ['a', 'b']):
+            pm.importFile(v, namespace=namespace)
+            pm.select('C_playerCamera_CAM')
+            mel.eval('lookThroughModelPanelClipped C_playerCamera_CAM modelPanel4 0.001 1000')
+            pm.geometryConstraint('C_groundDrive_PLY', 'C_main_CTL')
+            pm.geometryConstraint('C_groundDrive_PLY', 'C_front_CTL')
+            pm.delete(pm.parentConstraint('StartLocation', 'C_main_CTL'))
+            pm.delete(pm.parentConstraint('StartLocation', 'C_front_CTL'))
+
+            v = vehicle.Vehicle(transform='C_main_CTL',
+                                front_aim='C_front_CTL',
+                                body='C_body_CTL',
+                                steering_axis='C_steeringAxis_CTL',
+                                steering_wheel='C_steeringWheel_CTL',
+                                ui_pointer='C_pointer_CTL',
+                                ui_health='C_health_CTL',
+                                ui_countdown=['C_3_PLY', 'C_2_PLY', 'C_1_PLY', 'C_go_PLY'],
+                                wheels=['L_frontWheel_CTL', 'R_frontWheel_CTL',
+                                          'L_rearWheel_CTL', 'R_rearWheel_CTL'],
+                                track_curve='C_center_CRVShape',
+                                collider=gameobject.SphereCollider('C_Collider_COL'),
+                                fire=gameobject.Particle('C_fire_PAR', particle_shape='C_fire_PARShape'))
+            game_manager.vehicles.append(v)
+            pm.refresh()
+        # end for
+
+        # TODO MOdifty the view properly
+        # modelEditor -e -locators false modelPanel4;
+        pm.mel.eval('displayStyle -textured')
+
+        gameobject.SphereCollider('sphere1')
+
     # end def on_start
 
     def on_stop(self):
         """Override to create and initialize GameObjects on end."""
-        print('ENDED RACEGAME')
+        print 'ENDED RACEGAME'
     # end def on_stop
 # end class RaceGame
 
@@ -76,61 +165,31 @@ class GameManager(gameobject.GameObject):
     def __init__(self, transform):
         """Initialize GameManager."""
         super(GameManager, self).__init__(transform)
+        self.vehicles = list()
+        self.started = False
     # end def __init__
 
     def start(self):
         """@todo documentation for start."""
-        self._assemble_scene()
-        # loadingdialog.LoadingDialog(self._assemble_scene, 'Loading Game ...')
-        # pm.refresh()
-        # 3. Show start Countdown
-        # loadingdialog.LoadingDialog(partial(time.sleep, 1), '3')
-        # loadingdialog.LoadingDialog(partial(time.sleep, 1), '2')
-        # loadingdialog.LoadingDialog(partial(time.sleep, 1), '1')
-    # end def start
-
-    def _assemble_scene(self):
-        """@todo documentation for _assemble_scene."""
-        return
-        course = '/corp/home/pschweizer/Desktop/track.ma'
-        vehicles = ['/corp/home/pschweizer/Desktop/scoopAsCar.ma']
-        pm.newFile(f=True)
-        pm.importFile(course)
-        for v in vehicles:
-            pm.importFile(v)
-
-            # TODO Attach the car to the surface properly
-
-            # TODO Add a name tag to the car
-
+        for v in self.vehicles:
+            v.update(0.001)
         # end for
 
-        # TODO Look through Camera
-        pm.select('player1_CAM')
-        mel.eval('lookThroughModelPanelClipped player1_CAM modelPanel4 0.001 1000')
+        # 3. Show start Countdown
+        for i in range(4):
+            for v in self.vehicles:
+                v.ui_countdown[i].setAttr('v', True)
+            # end for
+            pm.refresh()
+            time.sleep(1)
+            for v in self.vehicles:
+                v.ui_countdown[i].setAttr('v', False)
+            # end for
+        # end for
+    # end def start
 
-        from rnkRig.animation.objectonsurface import objectonsurface
-        pm.delete(pm.parentConstraint('startingPosition1', 'C_global_CTL'))
-        objectonsurface.ObjectOnSurface('C_mainBodyContact_M', 'course').run()
-        objectonsurface.ObjectOnSurface('L_frontWheelContact_M', 'course').run()
-        objectonsurface.ObjectOnSurface('R_frontWheelContact_M', 'course').run()
-        objectonsurface.ObjectOnSurface('L_rearWheelContact_M', 'course').run()
-        objectonsurface.ObjectOnSurface('L_rearWheelContact_M', 'course').run()
-
-
-        #### GAME ENGINE STUFF BELOW ###########################
-
-        # TODO Properly register the Vehicles
-        scoop = vehicle.Vehicle(transform='C_vehicleDrive0_CTL',
-                                front_axis='C_frontWheelMainSteer_CTL',
-                                steering_wheel='C_steerWheel0_CTL',
-                                wheels=['L_frontWheelRoll_CTL',
-                                        'L_rearWheelRoll_CTL',
-                                        'R_frontWheelRoll_CTL',
-                                        'R_rearWheelRoll_CTL'],
-                                overall_scale=50,
-                                max_steering_angle=20,
-                                track_curve='trackCurveShape',
-                                collider='sphereCollider')
-    # end def _assemble_scene
+    def update(self, delta_time):
+        """@todo documentation for update."""
+        pass
+    # end def update
 # end class GameManager
